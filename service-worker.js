909175725eb2ad801e6da6318601fbb4bf42977e
bowlperson <61048483+bowlperson@users.noreply.github.com>
2025-09-04 08:32:57 -0700

    chore: remove notification button

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = 'taskforge-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
