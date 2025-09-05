b8976405c347dda915de3e52145ed4c6819ce2ab
bowlperson <61048483+bowlperson@users.noreply.github.com>
2025-09-05 08:48:32 -0700

    Remove Firebase auth and restore local storage persistence

--- service-worker.js
+++ service-worker.js
@@ -7,17 +7,5 @@ self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 self.addEventListener('fetch', e => {
-  const url = e.request.url;
-  if (url.includes('firebasestorage.googleapis.com') || url.includes('storage.googleapis.com')) {
-    e.respondWith(
-      caches.open(CACHE).then(cache =>
-        cache.match(e.request).then(r => r || fetch(e.request).then(resp => {
-          cache.put(e.request, resp.clone());
-          return resp;
-        }))
-      )
-    );
-    return;
-  }
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
